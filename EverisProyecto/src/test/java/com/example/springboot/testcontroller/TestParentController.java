6638f2605327c50b4ce7279da825542ba298ac97
zigotero <jashirchirre@gmail.com>
2019-07-12 16:26:02 -0500

    test controller
    testeo de controler

--- EverisProyecto/src/test/java/com/example/springboot/testcontroller/TestParentController.java
+++ EverisProyecto/src/test/java/com/example/springboot/testcontroller/TestParentController.java
@@ -1,51 +1,48 @@
 package com.example.springboot.testcontroller;
 
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.example.springboot.controller.FamiliesController;
 
+import com.example.springboot.controller.ParentsController;
 
-
-@RunWith(SpringJUnit4ClassRunner.class)
-public class TestFamilyController {
-
+public class TestParentController {
+	
 
 	//testea todo lo relacionado al llamado de servlets
 	//llamar directamente los endpoints
 	@Autowired
 	private MockMvc mocMvc;
 	//inyeccion
-	@InjectMocks
-	private FamiliesController familiesController;
+	//@InjectMocks
+	@MockBean
+	private ParentsController parentsController;
 
 	//creacion del mock  familiescontroller
 	@Before
 	public void  setUp() throws Exception{
 		
 		mocMvc= MockMvcBuilders
-				.standaloneSetup(familiesController)
+				.standaloneSetup(parentsController)
 				.build();
 	}
 	
 	@Test
-	public void testTamiliesController( )throws Exception{
+	public void testParentsController( )throws Exception{
 		
 		//testear el estado de los endpoints
-		mocMvc.perform(MockMvcRequestBuilders.get("/family/all"))
+		mocMvc.perform(MockMvcRequestBuilders.get("/parents/all"))
 		.andExpect(MockMvcResultMatchers.status().isOk())
 		;
 		
 		
 	}
-	
+
 }
